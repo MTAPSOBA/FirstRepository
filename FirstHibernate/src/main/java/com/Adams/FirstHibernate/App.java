e95eb831a401280eb32f03be9aab4d033c09e5ed
MTAPSOBA <142935971+MTAPSOBA@users.noreply.github.com>
2023-09-08 17:31:23 -0500

    last commit

---
+++ FirstHibernate/src/main/java/com/Adams/FirstHibernate/App.java
@@ -0,0 +1,13 @@
+package com.Adams.FirstHibernate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
